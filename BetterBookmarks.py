e14e04b0d6d0a16c77230e8eed434c6a14b77246
unknown <allray@d7b667.na.SAS.com>
2015-10-13 16:01:02 -0400

    BROKEN: Trying to get the marks to save is hard.

--- BetterBookmarks.py
+++ BetterBookmarks.py
@@ -1,30 +1,53 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, os
 import collections
+import json
 
 global settings
 settings = sublime.load_settings("BetterBookmarks.sublime-settings")
+
+def should_bookmark(view, region):
+	bookmarks = view.get_regions("bookmarks")
+	line = view.line(region)
+
+	for bookmark in bookmarks:
+		if line.contains(bookmark):
+			return False
+
+	return True
 
 class BetterBookmarksCommand(sublime_plugin.TextCommand):
 	def __init__(self, edit):
 		sublime_plugin.TextCommand.__init__(self, edit)
 
-		self.bookmarks = []
+		directory = "{:s}/User/BetterBookmarks".format(sublime.packages_path())
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		
+		bookmark_cache_name = sublime.expand_variables("${file_base_name}.sublime-settings", sublime.active_window().extract_variables())
+		self.bookmark_cache = "BetterBookmarks/{:s}".format(bookmark_cache_name)
+		self.cache = sublime.load_settings(self.bookmark_cache)
+
 		self.layers = collections.deque(settings.get("layer_icons"))
 		self.layer = settings.get("default_layer")
+		self.bookmarks = self.cache.get("Bookmarks")
+		if not self.bookmarks:
+			self.bookmarks = dict([])
 		self.refresh_bookmarks()
 
 	def add_marks(self, list):
 		icon = settings.get("layer_icons")[self.layer]["icon"]
 		scope = settings.get("layer_icons")[self.layer]["scope"]
 
-		self.view.add_regions(self.layer, list, scope, icon, sublime.PERSISTENT | sublime.HIDDEN)
+		self.view.add_regions("bookmarks", list, scope, icon, sublime.PERSISTENT | sublime.HIDDEN)
 
 		self.refresh_bookmarks()
 
 	def refresh_bookmarks(self):
-		self.bookmarks = self.view.get_regions(self.layer)
+		print(self.bookmarks)
+		self.bookmarks[self.layer] = self.view.get_regions("bookmarks")
+		print(self.bookmarks)
 
-	def bookmark_line(self, view, layer, line):
+	def bookmark_line(self, layer, line):
 		bookmarks = self.bookmarks
 		newMarks = []
 		bookmarkFound = False
@@ -43,37 +66,47 @@ class BetterBookmarksCommand(sublime_plugin.TextCommand):
 
 		self.add_marks(newMarks)
 
-	def should_bookmark(self, view, region):
-		bookmarks = view.get_regions("bookmarks")
-		line = view.line(region)
+	def swap_layers(self, direction):
+		if direction == "prev":
+			self.layers.rotate(-1)
+		elif direction == "next":
+			self.layers.rotate(1)
 
-		for bookmark in bookmarks:
-			if line.contains(bookmark):
-				return False
+		if settings.get("auto_save_marks"):
+#			layer_dict = dict([])
+#			for layer in self.layers:
+#				layer_dict[layer] = self.bookmarks
+			print(self.bookmarks)
+			self.cache.set("Bookmarks", {self.bookmarks})
+			sublime.save_settings(self.bookmark_cache)
 
-		return True
+		self.layer = self.layers[0]
+		sublime.status_message(self.layer)
+		self.refresh_bookmarks()
 
 	def run(self, edit, **args):
 		if args.get('bookmark_line'):
-			self.bookmark_line(self.view, self.layer, self.view.line(self.view.sel()[0]))
-		if args.get('swap_layers'):
-			direction = args.get('direction')
-
-			if direction == "prev":
-				self.layers.rotate(-1)
-			elif direction == "next":
-				self.layers.rotate(1)
-
-			self.layer = self.layers[0]
-			self.refresh_bookmarks()
-		if args.get('clear_marks'):
+			self.bookmark_line(self.layer, self.view.line(self.view.sel()[0]))
+		elif args.get('swap_layers'):
+			self.swap_layers(args.get('direction'))
+		elif args.get('clear_marks'):
 			self.add_marks([])
 
 class BetterBookmarksEventListener(sublime_plugin.EventListener):
+	def __init__(self):
+		sublime_plugin.EventListener.__init__(self)
+
 	def on_load(self, view):
 		if settings.get("load_marks_on_load"):
 			print("Implement - On load")
 
-	def on_post_save(self, view):
-		if settings.get("save_marks_on_save"):
-			print("Implement - On save")
+	# def on_post_save(self, view):
+	# 	if settings.get("save_marks_on_save"):
+	# 		bookmarks = view.get_regions("bookmarks")
+			# if not len(bookmarks):
+			# 	filename = "BetterBookmarks/{:s}.sublime-settings".format(self.bookmark_cache)
+			# 	marks = sublime.load_settings(filename)
+			# 	print(self.bb.layers)
+				# for layer in self.bb.layers:
+				# 	print(layer)
+				# marks.set()
